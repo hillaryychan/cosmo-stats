1e847c05dbf4f13b04ae61cd6ffdb3afcf01f5ce
Hillary Chan <hillary.chan91@gmail.com>
2025-04-07 21:36:12 +1000

    add argparser and positional arg for season

--- src/cosmo_stats/__init__.py
+++ src/cosmo_stats/__init__.py
@@ -1,12 +1,28 @@
+import argparse
 import asyncio
+import sys
 
 from cosmo_stats.enums import Season
 from cosmo_stats.objekts.service import default_objekt_service
 
 
+class CosmoStatsArgsNamespace:
+    season: Season
+
+
 def main() -> None:
+    args = CosmoStatsArgsNamespace()
+
+    parser = argparse.ArgumentParser(prog="cosmo-stats")
+    parser.add_argument(
+        "season",
+        choices=[season.value for season in Season],
+        type=str.capitalize,
+        help="The season of the objekts",
+    )
+    parser.parse_args(sys.argv[1:], namespace=CosmoStatsArgsNamespace)
     asyncio.run(
         default_objekt_service.get_objekt_sales_stats(
-            Season.EVER01, ["117Z", "118Z", "119Z", "120Z"]
+            args.season, ["117Z", "118Z", "119Z", "120Z"]
         )
     )
