bdd43a0e9883445c053dfbc3872c28f0de5dd5e7
Hillary Chan <hillary.chan91@gmail.com>
2025-10-07 17:53:21 +1100

    add idntt Autumn25 season

--- src/cosmo_stats/enums/idntt.py
+++ src/cosmo_stats/enums/idntt.py
@@ -6,6 +6,7 @@ from cosmo_stats.enums.cosmo import Season
 class IdnttSeason(Season):
     SPRING25 = "Spring25"
     SUMMER25 = "Summer25"
+    AUTUMN25 = "Autumn25"
 
 
 class IdnttObjektClass(StrEnum):
